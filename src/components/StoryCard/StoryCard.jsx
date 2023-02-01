89728f742297c6113bc7e11170ed20fba7ee97af
Paolo Debuque <pdebuque@gmail.com>
2023-02-01 15:59:51 -0600

    contact list item static

---
+++ src/components/StoryCard/StoryCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Box, Typography} from '@mui/material';
+
+export default function StoryCard({ story }) {
+  return (
+    <Box>
+      <Typography variant = 'body2'>{story.title}</Typography>
+      <Typography variant = 'body2'>{story.publication_date}</Typography>
+    </Box>
+  )
+}

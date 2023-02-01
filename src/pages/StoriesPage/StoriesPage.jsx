a64c237d695246b4285657ba2c78f18261cfda4b
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2023-02-01 13:28:54 -0600

    title and tags in place

--- src/pages/StoriesPage/StoriesPage.jsx
+++ src/pages/StoriesPage/StoriesPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { story } from '../../sampleStoryData';
+import { Box, Grid, Typography } from '@mui/material';
+import ListTags from '../../components/ListTags/ListTags';
 
-export default function StoriesPage(){
+export default function StoriesPage() {
     return (
-        <>
-            <h1>Stories Page</h1>
-        </>
+        <Box>
+            <Grid container space={1}>
+                <Grid item xs={9}>
+                    <Typography variant='h4'>{story.title}</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                    <Box>
+                        <ListTags numOfDisplay={4} tags={story.tags}/>
+                        {/* {story.tags.map((tag) => {
+                            return (
+                                <Typography variant='body1' component='button' sx={{ backgroundColor: '#951C2A', color: '#FFFFFF', border: 'none', m: .5, borderRadius: 1 }}>
+                                    {tag.name}
+                                </Typography>
+                            )
+                        })} */}
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }

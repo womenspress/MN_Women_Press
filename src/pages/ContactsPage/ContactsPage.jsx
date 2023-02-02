c2fa66a81bd6724846cecafa566a3c6eebd361fa
AnthonyDampier <Dampieranthony2@gmail.com>
2023-02-02 17:48:30 -0600

    nav update

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import CreateNewContactModal from '../../components/CreateNewContactModal/CreateNewContactModal';
 
 export default function ContactsPage(){
     return(
     <>
-        <h1>Contacts Page</h1>
+        <div>
+            <Typography variant="h3" component="h1">
+                Heading
+            </Typography>
+            <CreateNewContactModal/>
+            
+        </div>
     </>
     )
 }

e54d11ebef38347b77d4a6b5d040fb1c09b236fd
Victoria Mertens <victoria.mertens44@gmail.com>
2023-02-03 12:46:18 -0600

    Put up and functioning

--- server/routes/stories.router.js
+++ server/routes/stories.router.js
@@ -62,28 +62,32 @@ router.post('/', async (req, res) => {
   console.log('In post route', req.body);
   console.log(req.body.tags);
   // POST route code here
-  //themes aren't applicable to this process
-  //let theme = req.body.theme;
   let story = req.body;
   let tags = req.body.tags;
   let contacts = req.body.contacts;
-  let postStoryQuery = `INSERT INTO "story" 
+  let postStoryQuery = `
+  INSERT INTO "story" 
   ("title", "subtitle", "article_text", "article_link", "notes", "type", "copies_sent", "photo_uploaded", 
   "fact_checked", "graphic_image_required", "external_link", "word_count", "date_added", "rough_draft_deadline",
   "final_draft_deadline", "publication_date", "photo_required", "fact_check_required","graphic_image_completed")
   VALUES 
   ($1 ,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19)
   RETURNING "id";`; //Return id of story
+
+  //I am building this under the assumption that all tags and contacts that are attached are already in the tags table.
   let attachTagsQuery = `
   INSERT INTO "story_tag"
   ( "tag_id","story_id")
   VALUES 
   ($1,$2);`;
-  //I am building this under the assumption that all tags and contacts that are attached are already in the tags table.
-  let postContactsQuery = `INSERT INTO "story_contact" 
+
+  let postContactsQuery = `
+  INSERT INTO "story_contact" 
   ("contact_id","story_id","invoice_paid", "invoice_total", "project_association") 
   VALUES 
   ($1, $2, $3, $4, $5);`;
+
+  //Query
   const connection = await pool.connect();
   try {
     await connection.query('BEGIN;');
@@ -157,13 +161,91 @@ router.delete('/:id', (req, res) => {
 /**
  * EDIT route template
  */
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // EDIT route code here
   let id = req.params.id;
   let story = req.body;
-  let updateStoryQueryText = 'SQL';
-  pool.query(updateStoryQueryText, [id, story.name]).then().catch();
-  res.sendStatus(200);
+  let deleteTagsQuery = `DELETE FROM "story_tag" WHERE "story_id" = $1;`;
+  let deleteContactsQuery = `DELETE FROM "story_contact" WHERE "story_id" = $1;`;
+  let updateStoryQueryText = `
+  UPDATE "story"
+  SET
+  "title" = $1, "subtitle"= $2, "article_text"= $3, "article_link"= $4, "notes"= $5, "type"= $6, "copies_sent"= $7, "photo_uploaded"= $8, 
+  "fact_checked"= $9, "graphic_image_required"= $10, "external_link"= $11, "word_count"= $12, "date_added"= $13, "rough_draft_deadline"= $14,
+  "final_draft_deadline"= $15, "publication_date"= $16, "photo_required"= $17, "fact_check_required"= $18,"graphic_image_completed"= $19
+  WHERE "id" = $20;`;
+  let updateStoryData = [
+    story.title, //1
+    story.subtitle, //2
+    story.article_text, //3
+    story.article_link, //4
+    story.notes, //5
+    story.type, //6
+    story.copies_sent, //7
+    story.photo_uploaded, //8
+    story.fact_check_completed, //9, same as fact_checked, different naming conventions in data
+    story.graphic_image_required, //10
+    story.external_link, //11
+    story.word_count, //12
+    story.date_added, //13
+    story.rough_draft_deadline, //14
+    story.final_draft_deadline, //15
+    story.publication_date, //16
+    //story.photo, // will put in when exisits in DB
+    //y.copies_required, //will put in when exisits in DB
+    story.photo_required, //17
+    story.fact_check_required, //18
+    story.graphic_image_completed, //19
+    //story.payment_required,//will add when exisits in DB
+    //story.payment_completed, will add when exisits in DB
+    id,
+  ];
+  let attachTagsQuery = `
+  INSERT INTO "story_tag"
+  ( "tag_id","story_id")
+  VALUES 
+  ($1,$2);`;
+
+  let attachContactsQuery = `
+  INSERT INTO "story_contact" 
+  ("contact_id","story_id","invoice_paid", "invoice_total", "project_association") 
+  VALUES 
+  ($1, $2, $3, $4, $5);`;
+  //Query
+  const connection = await pool.connect();
+  try {
+    await connection.query('BEGIN;');
+    console.log('In query');
+    await connection.query(deleteTagsQuery, [id]);
+    console.log('before delete');
+    await connection.query(deleteContactsQuery, [id]);
+    console.log('before update');
+    await connection.query(updateStoryQueryText, updateStoryData);
+    console.log('before tags');
+    let tags = req.body.tags;
+    for (let tag of tags) {
+      await connection.query(attachTagsQuery, [tag.id, id]);
+    }
+    console.log('before contacts');
+    let contacts = req.body.contacts;
+    for (let contact of contacts) {
+      await connection.query(attachContactsQuery, [
+        contact.id,
+        id,
+        contact.invoice_paid,
+        contact.invoice_total,
+        contact.project_association,
+      ]);
+    }
+    await connection.query('COMMIT');
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    await connection.query('ROLLBACK');
+    res.sendStatus(500);
+  } finally {
+    await connection.release();
+  }
 });
 
 // require ID for params and req.body to include tags
@@ -181,8 +263,10 @@ router.post('/tag/:id', (req, res) => {
       let id = response.rows[0].id;
       res.send(id);
     })
-    .catch();
-  res.sendStatus(200);
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
 });
 
 //Created with the idea that the tag id was params and story id is in the body of the request

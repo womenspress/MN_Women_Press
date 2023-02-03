ab37ccbe65e3233c0a0987842cf3b0a51985b085
Victoria Mertens <victoria.mertens44@gmail.com>
2023-02-03 10:27:14 -0600

    Stories Post working with my current database

--- server/routes/stories.router.js
+++ server/routes/stories.router.js
@@ -29,34 +29,77 @@ router.get('/current/:id', (req, res) => {
  * POST route template
  */
 router.post('/', async (req, res) => {
+  console.log('In post route', req.body);
+  console.log(req.body.tags);
   // POST route code here
   //themes aren't applicable to this process
-  //let theme = req.theme;
-  let tags = req.tags;
-  let contacts = req.contacts;
-  let postStoryQuery = 'SQL'; //Return id of story
-  let attachTagsQuery = 'SQL';
+  //let theme = req.body.theme;
+  let story = req.body;
+  let tags = req.body.tags;
+  let contacts = req.body.contacts;
+  let postStoryQuery = `INSERT INTO "story" 
+  ("title", "subtitle", "article_text", "article_link", "notes", "type", "copies_sent", "photo_uploaded", 
+  "fact_checked", "graphic_image_required", "external_link", "word_count", "date_added", "rough_draft_deadline",
+  "final_draft_deadline", "publication_date", "archived")
+  VALUES 
+  ($1 ,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+  RETURNING "id";`; //Return id of story
+  let attachTagsQuery = `
+  INSERT INTO "story_tag"
+  ( "tag_id","story_id")
+  VALUES 
+  ($1,$2);`;
   //I am building this under the assumption that all tags and contacts that are attached are already in the tags table.
-  let postContactsQuery = 'SQL';
+  let postContactsQuery = `INSERT INTO "story_contact" 
+  ("contact_id","story_id","invoice_paid", "invoice_total", "project_association") 
+  VALUES 
+  ($1, $2, $3, $4, $5);`;
   const connection = await pool.connect();
   try {
     await connection.query('BEGIN;');
-    let storyId = await connection.query(postStoryQuery);
+    console.log('In query');
+    let storyResponse = await connection.query(postStoryQuery, [
+      story.title,
+      story.subtitle,
+      story.article_text,
+      story.article_link,
+      story.notes,
+      story.type,
+      story.copies_sent,
+      story.photo_uploaded,
+      story.fact_checked,
+      story.graphic_image_required,
+      story.external_link,
+      story.word_count,
+      story.date_added,
+      story.rough_draft_deadline,
+      story.final_draft_deadline,
+      story.publication_date,
+      story.archived,
+    ]);
+    console.log('toryId:', storyResponse.rows[0].id);
+    let storyId = storyResponse.rows[0].id;
     for (let tag of tags) {
-      await connection.query(attachTagsQuery, tag.id, storyId);
+      await connection.query(attachTagsQuery, [tag.id, storyId]);
     }
     for (let contact of contacts) {
-      await connection.query(postContactsQuery, contact.id, storyId);
+      await connection.query(postContactsQuery, [
+        contact.id,
+        storyId,
+        contact.invoice_paid,
+        contact.project_association,
+        contact.invoice_amount,
+      ]);
     }
     await connection.query('COMMIT;');
+    res.sendStatus(200);
   } catch (err) {
+    console.log(err);
     await connection.query('ROLLBACK');
     res.sendStatus(500);
   } finally {
     connection.release();
   }
-
-  res.sendStatus(200);
 });
 
 /**

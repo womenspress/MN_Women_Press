7613f2ceba1fafb802fe49f1134748640845102f
Brett R. Gebbie <brg078@gmail.com>
2023-02-04 14:10:11 -0600

    rolerouterup

--- server/routes/roles.router.js
+++ server/routes/roles.router.js
@@ -2,9 +2,15 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-
+//roles router built on the assumption that a roles table will be constructed for the client, thus only a get request is required, will add more if need be
 router.get('/', (req, res) => {
-    console.log('in get roles router')
+    const getAllRolesQuery = `SELECT * FROM "role" ORDER BY "name" ASC;`;
+    pool.query(getAllRolesQuery).then((results) => {
+        res.send(results.rows);
+    }).catch((err) => {
+        console.log('error in get all roles query: ', err)
+        res.sendStatus(500)
+    })
 })
 
 

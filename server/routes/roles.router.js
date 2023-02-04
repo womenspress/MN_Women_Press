5247ea2b27edfcdc26894b0fc6fbc75e39d1b55b
Brett R. Gebbie <brg078@gmail.com>
2023-02-04 14:05:25 -0600

    tagsrouterup

---
+++ server/routes/roles.router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    console.log('in get roles router')
+})
+
+
+
+
+module.exports = router;

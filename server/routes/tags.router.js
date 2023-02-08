6464b10d6e8e0bdbe1d8f50b7f5133a7b7388c31
Josh-Clemons <110069447+Josh-Clemons@users.noreply.github.com>
2023-02-08 14:08:54 -0600

    reject unauthorized middleware created and in place

--- server/routes/tags.router.js
+++ server/routes/tags.router.js
@@ -4,47 +4,47 @@ const router = express.Router();
 const {
     rejectUnauthenticated,
     rejectUnauthorized,
-  } = require('../modules/authentication-middleware');
+} = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
-  const getAllTagsQuery = `SELECT * FROM "tag" ORDER BY "name" ASC;`;
-  pool
-    .query(getAllTagsQuery)
-    .then((results) => {
-      res.send(results.rows);
-    })
-    .catch((err) => {
-      console.log('error in get all tags query: ', err);
-      res.sendStatus(500);
-    });
+router.get('/',  rejectUnauthenticated, rejectUnauthorized,(req, res) => {
+    const getAllTagsQuery = `SELECT * FROM "tag" ORDER BY "name" ASC;`;
+    pool
+        .query(getAllTagsQuery)
+        .then((results) => {
+            res.send(results.rows);
+        })
+        .catch((err) => {
+            console.log('error in get all tags query: ', err);
+            res.sendStatus(500);
+        });
 });
 
-router.post('/', (req, res) => {
-  const getAllTagsQuery = `INSERT INTO "tag"("name", "description") VALUES ($1,$2) RETURNING "id";`;
-  pool
-    .query(getAllTagsQuery, [req.body.name, req.body.description])
-    .then((response) => {
-      let tagId = response.rows[0];
-      console.log('TAGID:', tagId);
-      res.send(tagId);
-    })
-    .catch((err) => {
-      console.log('error in add tag query: ', err);
-      res.sendStatus(500);
-    });
+router.post('/', rejectUnauthenticated, rejectUnauthorized, (req, res) => {
+    const getAllTagsQuery = `INSERT INTO "tag"("name", "description") VALUES ($1,$2) RETURNING "id";`;
+    pool
+        .query(getAllTagsQuery, [req.body.name, req.body.description])
+        .then((response) => {
+            let tagId = response.rows[0];
+            console.log('TAGID:', tagId);
+            res.send(tagId);
+        })
+        .catch((err) => {
+            console.log('error in add tag query: ', err);
+            res.sendStatus(500);
+        });
 });
 
-router.delete('/deletetag/:id', (req, res) => {
-  const deleteTagQuery = `DELETE FROM "tag" WHERE "id" = $1;`;
-  pool
-    .query(deleteTagQuery, [req.params.id])
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log('error in delete tag query: ', err);
-      res.sendStatus(500);
-    });
+router.delete('/deletetag/:id', rejectUnauthenticated, rejectUnauthorized, (req, res) => {
+    const deleteTagQuery = `DELETE FROM "tag" WHERE "id" = $1;`;
+    pool
+        .query(deleteTagQuery, [req.params.id])
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('error in delete tag query: ', err);
+            res.sendStatus(500);
+        });
 });
 
 module.exports = router;

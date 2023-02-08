84de6a9354c2b97349323458f51b87bd1f796a87
AnthonyDampier <Dampieranthony2@gmail.com>
2023-02-08 09:42:51 -0600

    final mui edit push

--- src/components/EditContactModal/EditContactModal.jsx
+++ src/components/EditContactModal/EditContactModal.jsx
@@ -23,7 +23,7 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 900,
-    height: 700,
+    height: 470,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
